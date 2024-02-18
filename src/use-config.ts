c6135f7ab32b74920f2ecd220c026a3b0e31226b
Eyal Keren <eyalk@monada.co>
2024-02-18 14:27:52 +0200

    removed some file

--- src/use-config.ts
+++
@@ -1,8 +0,0 @@
-import {useQuery} from "react-query";
-import axios from "axios";
-debugger;
-const configBaseUrl = "https://d1ja8b6ce1s4ul.cloudfront.net";
-const configPath = "/config.json";
-export const useConfig = () => {
-    return useQuery("config", async () => axios.get(configBaseUrl + configPath));
-}
