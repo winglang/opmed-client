79ac8efa4ba040b17a1f027ebc01a94906cae0a1
Eyal Keren <eyalk@monada.co>
2024-02-18 15:15:14 +0200

    moved url to params

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,13 +4,18 @@ import {Lists} from "./Lists";
 
 export const Layout = () => {
 
-    // const {data: config} = useConfig();
+  // Create URLSearchParams object
+  const searchParams = new URLSearchParams(new URL(window.location.href).search);
+
+  // Get specific query parameter
+  const url = searchParams.get('baseUrl') || "https://ma82rg0r1i.execute-api.us-east-1.amazonaws.com/prod"; // 'value1'
+
 
     return (
         <div className="min-h-screen bg-slate-800">
             <Header header={"Where To Eat"}/>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex flex-row w-full place-content-evenly">
-                <Lists baseUrl={"http://127.0.0.1:54379"}/>
+                <Lists baseUrl={url}/>
             </div>
         </div>
     )
