eac27a9593d3e4ef717d539a117fcde3a02a7e63
Eyal Keren <eyalk@monada.co>
2024-02-19 18:03:50 +0200

    removing traces of bookmarks

--- src/Lists.tsx
+++ src/Lists.tsx
@@ -8,25 +8,14 @@ export interface ListsProps {
 }
 export const Lists = ({baseUrl}: ListsProps) => {
 
-    const {useAddFavorite, useListFavorites, useListRecommended} = useApi({baseUrl});
-    const {data: favorites} = useListFavorites();
+    const {useListRecommended} = useApi({baseUrl});
     const {data: recommended} = useListRecommended();
-    const {mutate} = useAddFavorite();
-
-    const onBookmarkClick = (item: ListItem) => {
-        mutate(item);
-    }
 
     return (
         <>
             <div className="px-4 py-6 sm:px-0 flex flex-col">
                 <Title title={"Recommended"}/>
-                <List items={recommended?.data?.restaurants ?? []}
-                      actionButton={{
-                          icon: <BookmarkIcon className={"text-slate-50 w-4 hover:text-sky-600 cursor-pointer"}/>,
-                          title: "Add to favorite",
-                          onClick: onBookmarkClick
-                      }}/>
+                <List items={recommended?.data?.restaurants ?? []}/>
             </div>
         </>
     )
